b3a90aed571a50136f856e6685d43390c0a4220f
hansellher <hansellhernandez74@gmail.com>
2026-09-10 05:35:24 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,18 @@
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Apoya nuestro proyecto",
+  description: "Realiza una donación para apoyar nuestro proyecto.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
